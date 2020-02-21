6bbe876bff21a0e8a18c90898be24bc86975bf09
ahansrisuk <ahansrisuk@gmail.com>
2020-02-21 09:36:38 -0500

    cleanup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,15 +9,14 @@ import SEO from '../components/seo';
 const IndexPage = () => (
     <Layout>
         <SEO title="Home" />
-        <div className="h-screen sm:w-4/5 pb-4 flex flex-col justify-between pt-40" id="top">
+        <div className="h-screen flex flex-col justify-between pb-4 pt-40" id="top">
             {/* main message */}
-            <h2 className="text-4xl sm:text-6xl xl:text-max font-medium">FRONT END DEVELOPMENT, UX DESIGN & RESEARCH</h2>
-            <div className="bottom-0">
-                <Arrow target="#about"/>
-            </div>
+            <h2 className="text-4xl sm:w-4/5 sm:text-6xl xl:text-max font-medium">FRONT END DEVELOPMENT, UX DESIGN & RESEARCH</h2>
+            <Arrow target="#about"/>
         </div>
-        {/* information container*/}
+        {/* information container */}
         <About />
+        {/* selected works */}
         <Projects />
     </Layout>
 );
