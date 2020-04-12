8d3af4f0703a6271c5ed3eb86687c61b4c600f97
Allen Hansrisuk <ahansrisuk@email.com>
2020-04-12 19:51:54 -0400

    created langing page

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
 import Vue from 'vue';
 import VueAnime from 'vue-animejs';
+import VueTypedJs from 'vue-typed-js';
 import VueRouter from 'vue-router';
 
 import App from './App.vue';
 
 import './assets/tailwind.css';
 
-Vue.use(VueRouter)
-Vue.use(VueAnime)
+Vue.use(VueRouter);
+Vue.use(VueAnime);
+Vue.use(VueTypedJs);
 
 Vue.config.productionTip = false
 
+import Landing from './pages/Landing.vue'
+
+const routes = [
+    { path: '/', component: Landing }
+]
+
+const router = new VueRouter({
+    routes
+})
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router
 }).$mount('#app')
---
+++ src/pages/Landing.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="mt-48 m-auto w-full max-w-md">
+        <vue-typed-js 
+            :strings="['Hello.^800 I\'m Allen.']"
+            :typeSpeed="80" 
+            @onComplete="enterContent"
+            >
+            <h1 class="text-2xl">
+                <span class="typing"></span>
+            </h1>
+        </vue-typed-js>
+        <ul class="mt-8 mb-12" v-show="showContent">
+            <li>- Front End Developer</li>
+            <li>- UX/UI Designer</li>
+        </ul>
+        <button class="text-sm" v-show="showContent">
+            Let's create something together ... ->
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Landing',
+    data: function () {
+        return {
+            showContent: false,
+        }
+    },
+    methods: {
+        enterContent () {
+            this.showContent = true;
+            let targets = document.querySelectorAll('li');
+            this.$anime
+                .timeline()
+                .add ({
+                    targets: targets,
+                    delay: (el, i) => 800 + (i * 800),
+                    opacity: [0,1],
+                    translateY: [50, 0],
+                    easing: 'easeOutElastic'
+                })
+                .add ({
+                    targets: 'button',
+                    opacity: [0,1],
+                    duration: 1000,
+                    easing: 'easeInOutSine'
+                })
+        }
+    }
+}
+</script>
