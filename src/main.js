dd9a2a730acd6f8a063f107d8776bd0c76e72435
Allen Hansrisuk <ahansrisuk@email.com>
2020-04-12 17:55:39 -0400

    set up dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueAnime from 'vue-animejs';
+import VueRouter from 'vue-router';
+
+import App from './App.vue';
+
+import './assets/tailwind.css';
+
+Vue.use(VueRouter)
+Vue.use(VueAnime)
 
 Vue.config.productionTip = false
 
