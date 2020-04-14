e2cda1a1968d20ec486d4000e2eec50c547e59e7
Allen Hansrisuk <ahansrisuk@email.com>
2020-04-13 23:06:02 -0400

    ignore typed.js cursor styles

--- postcss.config.js
+++ postcss.config.js
@@ -15,6 +15,9 @@ defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
 // Whitelist auto generated classes for transitions and router links.
 // From: https://github.com/ky-is/vue-cli-plugin-tailwind
 whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/],
+
+// ignored Typed.js blinking cursor
+whitelist: ['typedjsBlink']
 });
 
 module.exports = {
