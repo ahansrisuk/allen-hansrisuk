f52bec737f4dfc0823206cb2f59fe0f8d19bc28d
Allen Hansrisuk <ahansrisuk@email.com>
2020-04-14 11:35:42 -0400

    form test

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,7 +6,7 @@
             method="post"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
-            action="/"
+            action="/#"
         >
             <input type="hidden" name="form-name" value="contact-form" />
 
