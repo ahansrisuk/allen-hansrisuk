4519c8fb6b21e3997d869d8538dcfc0a0cb7c2b6
Allen Hansrisuk <ahansrisuk@email.com>
2020-04-15 18:05:15 -0400

    update covid 19 page

--- src/pages/projects/COVID19Tracker.vue
+++ src/pages/projects/COVID19Tracker.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="bwh-font bg-light font-bold w-full absolute left-0 top-0 p-4 ">
-        <div class="max-w-2xl m-auto overflow-auto">
+        <div class="max-w-screen-lg m-auto overflow-auto">
             <router-link to="/projects" class="text-black font-bold code">&lt;- Back</router-link>
             <section class="rounded blue p-2 mt-8 text-white">
                 <h1 class="text-xl font-bold">COVID-19 Tracker</h1>
                 <span class="text-sm">UX/UI Design - Wireframing & Prototypes</span>
             </section>
             <section class="mt-4 pb-4 shadow-lg bg-gray-100 rounded">
-                <h2 class="underline blue text-white rounded p-1 pl-2 font-bold">Background</h2>
+                <h2 class="underline blue text-white rounded p-1 pl-2">Background</h2>
                 <p class="p-2">
                     In the midst of the COVID-19 outbreak, Brigham and Women's Hospital in Boston, MA wanted to 
                     explore a solution that would help contain the spread of COVID-19 and benefit the healthcare
@@ -15,16 +15,17 @@
                 </p>
             </section>
             <section class="mt-4 pb-4 shadow-lg bg-gray-100 rounded">
-                <h2 class="underline blue text-white rounded p-1 pl-2 font-bold">Requirements</h2>
+                <h2 class="underline blue text-white rounded p-1 pl-2">Requirements</h2>
                 <ul class="p-2">
                     <li>- Mobile app solution</li>
                     <li>- Mechanism to track provider burnout.</li>
                     <li>- Mechanism to report any symptoms related to COVID-19.</li>
                     <li>- Notifications for hospital protocol.</li>
+                    <li>- Build awareness and share helpful resources.</li>
                 </ul>
             </section>
             <section class="mt-4 pb-4 shadow-lg bg-gray-100 rounded">
-                <h2 class="underline blue text-white rounded p-1 pl-2 font-bold">Ideation</h2>
+                <h2 class="underline blue text-white rounded p-1 pl-2">Ideation</h2>
                 <img src="../../assets/covid-19-tracker/sketches.png" alt="COVID-19 Tracker Sketches" />
                 <p class="p-2">
                     Original sketches that outline the home screen, provider burnout interface and 
@@ -32,7 +33,7 @@
                 </p>
             </section>
             <section class="mt-4 pb-4 shadow-lg bg-gray-100 rounded">
-                <h2 class="underline blue text-white rounded p-1 pl-2 font-bold">Notification Screens</h2>
+                <h2 class="underline blue text-white rounded p-1 pl-2">Notification Screens</h2>
                 <img src="../../assets/covid-19-tracker/notifications.png" alt="COVID-19 Notifications"  class="p-2"/>
                 <p class="p-2">
                     Notifications to alert the user about recent news articles pertaining to the hospital, reminders
@@ -40,7 +41,7 @@
                 </p>
             </section>
             <section class="mt-4 pb-4 shadow-lg bg-gray-100 rounded">
-                <h2 class="underline blue text-white rounded p-1 pl-2 font-bold">Wireframes</h2>
+                <h2 class="underline blue text-white rounded p-1 pl-2">Wireframes</h2>
                 <img src="../../assets/covid-19-tracker/wireframes.png" alt="COVID-19 Tracker Wireframes"  class="p-2"/>
                 <p class="p-2">
                     Wireframes and prototype demonstrating user potential user journeys to hospital
