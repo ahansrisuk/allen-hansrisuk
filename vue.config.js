91de64337eb784c9c35de79781a6809775ac5299
Allen Hansrisuk <ahansrisuk@email.com>
2020-04-29 18:01:27 -0400

    prerender

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,23 @@ const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
 
 module.exports = {
-  plugins: [
-    new PrerenderSPAPlugin({
-      // Required - The path to the webpack-outputted app to prerender.
-      staticDir: path.join(__dirname, 'dist'),
-      // Required - Routes to render.
-      routes: ['/', '/about', '/some/deep/nested/route'],
-    }),
-  ],
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          // Required - The path to the webpack-outputted app to prerender.
+          staticDir: path.join(__dirname, 'dist'),
+          // Required - Routes to render.
+          routes: [
+            '/', 
+            '/about', 
+            '/projects',
+            '/contact',
+            '/projects/covid-19-tracker'
+          ],
+        }),
+      ],
+    }
+  }
 };
